e8dd31232e888c43444901ac9bdc694130e2438b
Miguel Sanchez <aleminapavon30@gmail.com>
2025-08-07 01:01:16 -0600

    database configuration

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import uvicorn
 import fastapi
+import json
+from utils.database import execute_query_json
 
 app = fastapi.FastAPI()
 
-@app.get("/version")
+@app.get("/")
 async def root():
-    return {"version": "0.0.0`"}
+    query = "SELECT * FROM pokequeue.messages;"
+    result = await execute_query_json(query)
+    result_dict = json.loads(result)
+    return result_dict
+
+@app.get("/version")
+async def version():
+    return {"version": "0.1.0`"}
 
 if __name__== "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
