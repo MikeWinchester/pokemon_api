c3e13e5bed287a2ed59dd28444bcb5e4c2cab9aa
Miguel Sanchez <aleminapavon30@gmail.com>
2025-08-06 22:24:11 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/version")
+async def root():
+    return {"version": "0.0.0`"}
+
+if __name__== "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
